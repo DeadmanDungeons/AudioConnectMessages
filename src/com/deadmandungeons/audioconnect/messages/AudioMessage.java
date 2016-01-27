3e23dbbf38d7accd9e22c9dce1295a09991d020f
Jon McPherson <jmcph3rs0n@gmail.com>
2016-01-26 19:47:36 -0500

    Adds isValid method to AudioFile, adds static parse method to Range, and renames transitionDelay to delayRange
    The audio file name validity check is now made by the public static isValid(String) method in AudioFile to allow
    checking for validity without creating an instance.
    The constructor for the simple Range class was made public and a public static parse(String) method was added
    to parse the result of the toString method which was also added.
    The transitionDelay property was renamed to delayRange to improve readability and understanding of the type
    for when it is serialized to JSON and interacted with through javascript.

--- src/com/deadmandungeons/audioconnect/messages/AudioMessage.java
+++ src/com/deadmandungeons/audioconnect/messages/AudioMessage.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 import com.deadmandungeons.connect.commons.Messenger.Message;
 import com.deadmandungeons.connect.commons.Messenger.MessageCreator;
 import com.deadmandungeons.connect.commons.Messenger.MessageType;
+import com.google.common.primitives.Ints;
 
 
 @MessageType("audio")
@@ -23,7 +24,7 @@ public class AudioMessage extends Message {
 	};
 	
 	private final Set<String> audioFiles;
-	private final Range transitionDelay;
+	private final Range delayRange;
 	private final boolean primary;
 	
 	
@@ -35,7 +36,7 @@ public class AudioMessage extends Message {
 		
 		private final UUID id;
 		private Set<String> audioFiles;
-		private Range transitionDelay;
+		private Range delayRange;
 		private boolean primary;
 		
 		protected Builder(UUID id) {
@@ -58,13 +59,16 @@ public class AudioMessage extends Message {
 			return this;
 		}
 		
-		public Builder transitionDelay(int seconds) {
-			transitionDelay = new Range(seconds, seconds);
-			return this;
+		public Builder delay(int seconds) {
+			return delayRange(seconds, seconds);
 		}
 		
-		public Builder transitionDelay(int minSeconds, int maxSeconds) {
-			transitionDelay = new Range(minSeconds, maxSeconds);
+		public Builder delayRange(int minSeconds, int maxSeconds) {
+			return delayRange(new Range(minSeconds, maxSeconds));
+		}
+		
+		public Builder delayRange(Range delayRange) {
+			this.delayRange = delayRange;
 			return this;
 		}
 		
@@ -83,13 +87,13 @@ public class AudioMessage extends Message {
 	protected AudioMessage(Builder builder) {
 		super(builder.id);
 		audioFiles = builder.audioFiles;
-		transitionDelay = builder.transitionDelay;
+		delayRange = builder.delayRange;
 		primary = builder.primary;
 	}
 	
 	
 	/**
-	 * @return the audio file source location of the audio file to play, or null if no audio should be played
+	 * @return the set of audio file source locations that should be played, or <code>null</code> if no audio should be played
 	 */
 	@Override
 	public Set<String> getData() {
@@ -97,10 +101,11 @@ public class AudioMessage extends Message {
 	}
 	
 	/**
-	 * @return
+	 * @return a range of integers that specify the minimum and maximum amount of seconds that the delay between
+	 * audio transitions should be. <code>null</code> will be returned if there should be no transition delay.
 	 */
-	public Range getTransitionDelay() {
-		return transitionDelay;
+	public Range getDelayRange() {
+		return delayRange;
 	}
 	
 	/**
@@ -123,7 +128,7 @@ public class AudioMessage extends Message {
 		private final int min;
 		private final int max;
 		
-		private Range(int min, int max) {
+		public Range(int min, int max) {
 			this.min = min;
 			this.max = max;
 		}
@@ -134,6 +139,30 @@ public class AudioMessage extends Message {
 		
 		public int getMax() {
 			return max;
+		}
+		
+		@Override
+		public String toString() {
+			return min + "-" + max;
+		}
+		
+		public static Range parse(String rangeStr) {
+			rangeStr = rangeStr.trim();
+			Integer minMax = Ints.tryParse(rangeStr);
+			if (minMax != null && minMax >= 0) {
+				return new Range(minMax, minMax);
+			} else {
+				String[] rangeArray = rangeStr.split("-");
+				if (rangeArray.length != 2) {
+					return null;
+				}
+				Integer min = Ints.tryParse(rangeArray[0].trim());
+				Integer max = Ints.tryParse(rangeArray[1].trim());
+				if (min == null || max == null || min > max) {
+					return null;
+				}
+				return new Range(min, max);
+			}
 		}
 		
 	}
@@ -151,6 +180,20 @@ public class AudioMessage extends Message {
 		}
 		
 		/**
+		 * @param audioFileName - The name of an audio file that was uploaded and stored on the AudioConnectWeb application
+		 * @return a new AudioFile instance representing a textually valid audio file stored locally on the webserver.
+		 * <code>null</code> will be returned if audioFileName is null or if it is invalid (See {@link #isValid(String)}).
+		 */
+		public static AudioFile create(String audioFileName) {
+			if (audioFileName != null) {
+				if (isValid(audioFileName)) {
+					return new AudioFile(audioFileName);
+				}
+			}
+			return null;
+		}
+		
+		/**
 		 * Valid file name characters are ASCII letters, digits, underscores, hyphens, and periods. A period character can
 		 * only be used between any other valid character, and is needed for separating the file extension suffix from the
 		 * name part. The file extension suffix is required, and must contain 1 to 8 ASCII letters or digits only.<br><br>
@@ -161,17 +204,12 @@ public class AudioMessage extends Message {
 		 * <tr><td>_file__name_.mp3</td><td>file_+_name.mp3</td></tr>
 		 * <tr><td>file.name.wav</td><td>file..name.wav</td></tr>
 		 * </table>
-		 * @param audioFileName - The name of an audio file that was uploaded and stored on the AudioConnect web application
-		 * @return a new AudioFile instance representing a textually valid audio file stored locally on the webserver.
-		 * <code>null</code> will be returned if the file name is either improperly structured, or contains illegal characters.
+		 * @param audioFileName - The name of an audio file that was uploaded and stored on the AudioConnectWeb application
+		 * @return <code>true</code> if the file name is valid and <code>false</code> if the file name is either improperly
+		 * structured, or contains illegal characters.
 		 */
-		public static AudioFile create(String audioFileName) {
-			if (audioFileName != null) {
-				if (FILE_NAME_PATTERN.matcher(audioFileName).matches()) {
-					return new AudioFile(audioFileName);
-				}
-			}
-			return null;
+		public static boolean isValid(String audioFileName) {
+			return FILE_NAME_PATTERN.matcher(audioFileName).matches();
 		}
 		
 	}
